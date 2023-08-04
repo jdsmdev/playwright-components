944959e42975b55b72f851156c31394142fb0d38
jdsmdev <jorge.soaresmoniz@hitachivantara.com>
2023-08-04 15:57:39 +0100

    feat(dialog): click button method

--- src/components/dialog.ts
+++ src/components/dialog.ts
@@ -10,8 +10,12 @@ export class DialogComponent {
   constructor(root: Page | Locator) {
     this.page = "page" in root ? root.page() : root;
     this.headingTitle = root.getByRole("heading").nth(0);
-    this.closeButton = root.getByRole("button", { name: "Close" });
+    this.closeButton = root.getByRole("button", { name: "close" });
     this.actionButton = root.locator("button[type='submit']");
-    this.cancelButton = root.getByRole("button", { name: "Cancel" });
+    this.cancelButton = root.getByRole("button", { name: "cancel" });
+  }
+
+  async click(name: string) {
+    await this.page.getByRole("button", { name }).click();
   }
 }
